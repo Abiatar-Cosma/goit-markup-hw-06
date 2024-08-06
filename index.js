f893f5c927afc320511e3388eead0f92f45b801c
Abiatar-Cosma <abiatar.cosma@yahoo.com>
2024-08-06 17:39:41 +0200

    initial commit 2

---
+++ index.js
@@ -0,0 +1,16 @@
+const button = document.querySelector('.order-button');
+const backdrop = document.querySelector('.modal-backdrop');
+
+button.addEventListener('click', toggleModal);
+
+backdrop.addEventListener('click', (e) => {
+    const target = e.target.closest('.modal-close-button');
+    if (!target) {
+        return
+    }
+    toggleModal()
+});
+
+function toggleModal() {
+    backdrop.classList.toggle("is-open");
+}
